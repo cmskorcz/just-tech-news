0e58c33459d13236353e28273cd75d5be4b7a7e0
Chase Skorczewski <chaseskorey@icloud.com>
2022-06-24 23:25:33 -0400

    added upvote model and routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const sequelize = require('../../config/connection');
+
+const { User, Post, Vote } = require('../../models');
 
 router.get('/', (req, res) => {
     User.findAll({
@@ -15,6 +17,18 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     User.findOne({
         attributes: { exclude: ['password'] },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ],
         where: {
             id: req.params.id
         }
